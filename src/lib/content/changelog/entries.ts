c1c6e765757bb8f2b475eb35b162dfb87b98dad5
oereor <unkn0wn___us3r@outlook.com>
2026-09-09 21:38:36 +0800

    Update entries.ts
    Fix a trailing comma issue.

--- src/lib/content/changelog/entries.ts
+++ src/lib/content/changelog/entries.ts
@@ -4,5 +4,5 @@ export const CHANGELOG_ENTRY_METADATA = [
   { id: '2026-09-04-search-update', date: '2026-09-04' },
   { id: '2026-09-07-i18n-update', date: '2026-09-07' },
   { id: '2026-09-09-add-init-av-stat', date: '2026-09-09' },
-  { id: '2026-09-09-fix-moc-season', date: '2026-09-09' },
+  { id: '2026-09-09-fix-moc-season', date: '2026-09-09' }
 ] as const;
